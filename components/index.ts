e7746dfedace65559ed9f59abe5e7c9c000a4af1
Abdulrahman <abd3lra7man.078@gmail.com>
2024-09-04 14:05:53 +0300

    add images in public folder and import it in components folder index.ts file.

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,56 @@
+// Components.
 import Hero from './Hero';
 
 export {
     Hero,
+}
+
+
+// Images.
+import Logo from '../public/logo.svg';
+import CarLogo from '../public/car-logo.svg';
+import DownArrow from '../public/arrow-down.svg';
+import RightArrow from '../public/right-arrow.svg';
+import Close from '../public/close.svg';
+import Discord from '../public/discord.svg';
+import Facebook from '../public/facebook.svg';
+import Linkedin from '../public/linkedin.svg';
+import Gas from '../public/gas.svg';
+import Github from '../public/github.svg';
+import HeartFilled from '../public/heart-filled.svg';
+import HeartOutline from '../public/heart-outline.svg';
+import HeroBg from '../public/hero-bg.png';
+import HeroImg from '../public/hero.png';
+import MagnifyingGlass from '../public/magnifying-glass.svg';
+import ModelIcon from '../public/model-icon.png';
+import Next from '../public/next.svg';
+import Pattern from '../public/pattern.png';
+import SteeringWheel from '../public/steering-wheel.svg';
+import Tire from '../public/tire.svg';
+import Twitter from '../public/twitter.svg';
+import Vercel from '../public/vercel.svg';
+
+export {
+    DownArrow,
+    RightArrow,
+    Logo,
+    CarLogo,
+    Close,
+    Discord,
+    Facebook,
+    Gas,
+    Github,
+    HeartFilled,
+    HeartOutline,
+    HeroImg,
+    HeroBg,
+    Linkedin,
+    MagnifyingGlass,
+    ModelIcon,
+    Next,
+    Pattern,
+    SteeringWheel,
+    Tire,
+    Twitter,
+    Vercel,
 }
